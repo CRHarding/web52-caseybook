fd00d29acac31c8dec981547c064285cef114405
Casey Harding <casey.r.harding@gmail.com>
2022-02-17 18:37:21 -0500

    Axios in place.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// 1.) Bring in useState, useEffect
+// 2.) Hit the api endpoint, and save the data in state
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://randomuser.me/api/?results=5")
+      .then(res => {
+        setUsers(res.data.results);
+      }).catch(err => console.error(err))
+  }, [])
   return (
     <div className="App">
       <h1>WELCOME TO CASEYBOOK!</h1>
